2241a3ece66e90971907b283a30d5f466dcdabb8
Tuomas Karna <tuomas.karna@intel.com>
2023-07-06 17:02:25 +0300

    remove array2d helper object

--- examples/mhp/wave-equation.cpp
+++ examples/mhp/wave-equation.cpp
@@ -9,35 +9,23 @@
 
 using T = double;
 
-template <typename T> class array2d {
-public:
-  array2d(std::size_t nx, std::std::size_t ny, dr::mhp::distribution dist)
-      : _nx(nx), _ny(ny), _mdarray({nx, ny}, dist) {}
-  array2d(std::std::size_t nx, std::std::size_t ny, T value,
-          dr::mhp::distribution dist)
-      : array2d(nx, ny, dist) {
-    dr::mhp::fill(_mdarray, value);
-  }
-  auto operator()(auto i, auto j) { return _mdarray[i * _ny + j]; }
-  void print(std::string msg) {
-    std::cout << msg << " (" << _nx << ", " << _ny << "): [";
-    for (std::std::size_t i = 0; i < _nx; i++) {
-      std::cout << "\n  ";
-      for (std::std::size_t j = 0; j < _ny; j++) {
-        std::cout << (*this)(i, j) << " ";
-      }
-    }
-    std::cout << "\n]\n";
-  }
-  std::std::size_t nx() { return _nx; }
-  std::std::size_t ny() { return _ny; }
-  dr::mhp::distributed_mdarray<T, 2> &md() { return _mdarray; }
+using Array = dr::mhp::distributed_mdarray<T, 2>;
 
-private:
-  std::size_t _nx;
-  std::size_t _ny;
-  dr::mhp::distributed_mdarray<T, 2> _mdarray;
-};
+void print_array(Array &a, std::string msg) {
+  auto nx = a.mdspan().extent(0);
+  auto ny = a.mdspan().extent(1);
+  const auto default_precision{std::cout.precision()};
+  std::cout << msg << " (" << nx << ", " << ny << "): [";
+  std::cout << std::fixed << std::setprecision(5);
+  for (std::size_t i = 0; i < nx; i++) {
+    std::cout << "\n  ";
+    for (std::size_t j = 0; j < ny; j++) {
+      std::cout << std::setw(9) << a.mdspan()(i, j) << " ";
+    }
+  }
+  std::cout << "\n]\n";
+  std::cout << std::setprecision(default_precision);
+}
 
 MPI_Comm comm;
 int comm_rank;
@@ -57,9 +45,9 @@ double exact_elev(double x, double y, double t, double lx, double ly) {
    */
   double amp = 0.5;
   double c = std::sqrt(g * h);
-  std::std::size_t n = 1;
+  std::size_t n = 1;
   double sol_x = std::cos(2 * n * M_PI * x / lx);
-  std::std::size_t m = 1;
+  std::size_t m = 1;
   double sol_y = std::cos(2 * m * M_PI * y / ly);
   double omega = c * M_PI * std::hypot(n / lx, m / ly);
   double sol_t = std::cos(2 * omega * t);
@@ -70,9 +58,9 @@ double initial_elev(double x, double y, double lx, double ly) {
   return exact_elev(x, y, 0.0, lx, ly);
 }
 
-void rhs(array2d<T> &u, array2d<T> &v, array2d<T> &e, array2d<T> &dudx,
-         array2d<T> &dvdy, array2d<T> &dudt, array2d<T> &dvdt, array2d<T> &dedt,
-         double g, double h, double dx_inv, double dy_inv, double dt) {
+void rhs(Array &u, Array &v, Array &e, Array &dudx, Array &dvdy, Array &dudt,
+         Array &dvdt, Array &dedt, double g, double h, double dx_inv,
+         double dy_inv, double dt) {
   /**
    * Evaluate right hand side of the equations
    */
@@ -81,32 +69,27 @@ void rhs(array2d<T> &u, array2d<T> &v, array2d<T> &e, array2d<T> &dudx,
     auto [in, out] = v;
     out(0, 0) = -dt * g * (in(1, 0) - in(0, 0)) * dx_inv;
   };
-  dr::mhp::stencil_for_each_2d({1, 1, 0, 0}, {0, 0}, rhs_dedx, e.md(),
-                               dudt.md());
+  dr::mhp::stencil_for_each_2d({1, 1, 0, 0}, {0, 0}, rhs_dedx, e, dudt);
   auto rhs_dedy = [dt, g, dy_inv](auto v) {
     auto [in, out] = v;
     out(0, 0) = -dt * g * (in(0, 1) - in(0, 0)) * dy_inv;
   };
-  dr::mhp::stencil_for_each_2d({0, 0, 0, 1}, {0, 1}, rhs_dedy, e.md(),
-                               dvdt.md());
+  dr::mhp::stencil_for_each_2d({0, 0, 0, 1}, {0, 1}, rhs_dedy, e, dvdt);
 
   auto rhs_dudx = [dt, h, dx_inv](auto v) {
     auto [in, out] = v;
     out(0, 0) = -dt * h * (in(0, 0) - in(-1, 0)) * dx_inv;
   };
-  dr::mhp::stencil_for_each_2d({1, 0, 0, 0}, {0, 0}, rhs_dudx, u.md(),
-                               dudx.md());
+  dr::mhp::stencil_for_each_2d({1, 0, 0, 0}, {0, 0}, rhs_dudx, u, dudx);
 
   auto rhs_dvdy = [dt, h, dy_inv](auto v) {
     auto [in, out] = v;
     out(0, 0) = -dt * h * (in(0, 1) - in(0, 0)) * dy_inv;
   };
-  dr::mhp::stencil_for_each_2d({1, 0, 0, 0}, {0, 0}, rhs_dvdy, v.md(),
-                               dvdy.md());
-
+  dr::mhp::stencil_for_each_2d({1, 0, 0, 0}, {0, 0}, rhs_dvdy, v, dvdy);
+  // FIXME fuse rhs_dudx/dvdy to a single kernel
   auto add = [](auto ops) { return ops.first + ops.second; };
-  dr::mhp::transform(dr::mhp::views::zip(dudx.md(), dvdy.md()),
-                     dedt.md().begin(), add);
+  dr::mhp::transform(dr::mhp::views::zip(dudx, dvdy), dedt.begin(), add);
 };
 
 int run(int n, bool benchmark_mode) {
@@ -126,8 +109,8 @@ int run(int n, bool benchmark_mode) {
   //   f---v---f---v---f---v---f---v---f-
 
   // number of cells in x, y direction
-  std::std::size_t nx = n;
-  std::std::size_t ny = n;
+  std::size_t nx = n;
+  std::size_t ny = n;
   const double xmin = -1, xmax = 1;
   const double ymin = -1, ymax = 1;
   const double lx = xmax - xmin;
@@ -136,7 +119,7 @@ int run(int n, bool benchmark_mode) {
   const double dy = ly / ny;
   const double dx_inv = 1.0 / dx;
   const double dy_inv = 1.0 / dy;
-  std::std::size_t halo_radius = 1;
+  std::size_t halo_radius = 1;
   auto dist = dr::mhp::distribution().halo(halo_radius);
 
   if (comm_rank == 0) {
@@ -157,7 +140,7 @@ int run(int n, bool benchmark_mode) {
   double alpha = 0.5;
   double dt = alpha * dx / c;
   dt = t_export / static_cast<int>(ceil(t_export / dt));
-  std::std::size_t nt = static_cast<int>(ceil(t_end / dt));
+  std::size_t nt = static_cast<int>(ceil(t_end / dt));
   if (benchmark_mode) {
     nt = 100;
     dt = 1e-5;
@@ -173,39 +156,43 @@ int run(int n, bool benchmark_mode) {
 
   // state variables
   // water elevation at T points
-  array2d<T> e(nx + 1, ny, 0.0, dist);
+  Array e({nx + 1, ny}, dist);
+  dr::mhp::fill(e, 0.0);
   // x velocity at U points
-  array2d<T> u(nx + 1, ny, 0.0, dist);
+  Array u({nx + 1, ny}, dist);
+  dr::mhp::fill(u, 0.0);
   // y velocity at V points
-  array2d<T> v(nx + 1, ny + 1, 0.0, dist);
+  Array v({nx + 1, ny + 1}, dist);
+  dr::mhp::fill(v, 0.0);
 
   // state for RK stages
-  array2d<T> e1(nx + 1, ny, 0.0, dist);
-  array2d<T> u1(nx + 1, ny, 0.0, dist);
-  array2d<T> v1(nx + 1, ny + 1, 0.0, dist);
-  array2d<T> e2(nx + 1, ny, 0.0, dist);
-  array2d<T> u2(nx + 1, ny, 0.0, dist);
-  array2d<T> v2(nx + 1, ny + 1, 0.0, dist);
+  Array e1({nx + 1, ny}, dist);
+  Array u1({nx + 1, ny}, dist);
+  Array v1({nx + 1, ny + 1}, dist);
+  Array e2({nx + 1, ny}, dist);
+  Array u2({nx + 1, ny}, dist);
+  Array v2({nx + 1, ny + 1}, dist);
 
   // time tendencies
-  array2d<T> dedt(nx + 1, ny, 0.0, dist);
-  array2d<T> dudt(nx + 1, ny, 0.0, dist);
-  array2d<T> dvdt(nx + 1, ny + 1, 0.0, dist);
+  // NOTE not needed if rhs kernels are fused with RK stage assignment
+  Array dedt({nx + 1, ny}, dist);
+  Array dudt({nx + 1, ny}, dist);
+  Array dvdt({nx + 1, ny + 1}, dist);
 
   // temporary arrays
-  // FIXME these should not be necessary
-  array2d<T> dudx(nx + 1, ny, dist);
-  array2d<T> dvdy(nx + 1, ny, dist);
+  // FIXME these are not necessary if dedt is evaluated in one go
+  Array dudx({nx + 1, ny}, dist);
+  Array dvdy({nx + 1, ny}, dist);
 
   // initial condition for elevation
-  for (std::size_t i = 1; i < e.nx(); i++) {
-    for (std::size_t j = 0; j < e.ny(); j++) {
+  for (std::size_t i = 1; i < e.mdspan().extent(0); i++) {
+    for (std::size_t j = 0; j < e.mdspan().extent(1); j++) {
       T x = xmin + dx / 2 + (i - 1) * dx;
       T y = ymin + dy / 2 + j * dy;
-      e(i, j) = initial_elev(x, y, lx, ly);
+      e.mdspan()(i, j) = initial_elev(x, y, lx, ly);
     }
   }
-  dr::mhp::halo(e.md()).exchange();
+  dr::mhp::halo(e).exchange();
 
   auto add = [](auto ops) { return ops.first + ops.second; };
   auto max = [](double x, double y) { return std::max(x, y); };
@@ -218,22 +205,21 @@ int run(int n, bool benchmark_mode) {
            2.0 / 3.0 * (std::get<1>(ops) + std::get<2>(ops));
   };
 
-  std::std::size_t i_export = 0;
+  std::size_t i_export = 0;
   double next_t_export = 0.0;
   double t = 0.0;
   double initial_v;
   auto tic = std::chrono::steady_clock::now();
-  for (std::std::size_t i = 0; i < nt + 1; i++) {
+  for (std::size_t i = 0; i < nt + 1; i++) {
     t = i * dt;
 
     if (t >= next_t_export - 1e-8) {
 
-      double elev_max = dr::mhp::reduce(e.md(), static_cast<T>(0), max);
-      double u_max = dr::mhp::reduce(u.md(), static_cast<T>(0), max);
+      double elev_max = dr::mhp::reduce(e, static_cast<T>(0), max);
+      double u_max = dr::mhp::reduce(u, static_cast<T>(0), max);
 
       double total_v =
-          (dr::mhp::reduce(e.md(), static_cast<T>(0), std::plus{}) + h) * dx *
-          dy;
+          (dr::mhp::reduce(e, static_cast<T>(0), std::plus{}) + h) * dx * dy;
       if (i == 0) {
         initial_v = total_v;
       }
@@ -259,39 +245,33 @@ int run(int n, bool benchmark_mode) {
     // step
     // RK stage 1: u1 = u + dt*rhs(u)
     rhs(u, v, e, dudx, dvdy, dudt, dvdt, dedt, g, h, dx_inv, dy_inv, dt);
-    dr::mhp::transform(dr::mhp::views::zip(u.md(), dudt.md()), u1.md().begin(),
-                       add);
-    dr::mhp::transform(dr::mhp::views::zip(v.md(), dvdt.md()), v1.md().begin(),
-                       add);
-    dr::mhp::transform(dr::mhp::views::zip(e.md(), dedt.md()), e1.md().begin(),
-                       add);
-    dr::mhp::halo(u1.md()).exchange();
-    dr::mhp::halo(v1.md()).exchange();
-    dr::mhp::halo(e1.md()).exchange();
+    dr::mhp::transform(dr::mhp::views::zip(u, dudt), u1.begin(), add);
+    dr::mhp::transform(dr::mhp::views::zip(v, dvdt), v1.begin(), add);
+    dr::mhp::transform(dr::mhp::views::zip(e, dedt), e1.begin(), add);
+    dr::mhp::halo(u1).exchange();
+    dr::mhp::halo(v1).exchange();
+    dr::mhp::halo(e1).exchange();
 
     // RK stage 2: u2 = 0.75*u + 0.25*(u1 + dt*rhs(u1))
     rhs(u1, v1, e1, dudx, dvdy, dudt, dvdt, dedt, g, h, dx_inv, dy_inv, dt);
-    dr::mhp::transform(dr::mhp::views::zip(u.md(), u1.md(), dudt.md()),
-                       u2.md().begin(), rk_update2);
-    dr::mhp::transform(dr::mhp::views::zip(v.md(), v1.md(), dvdt.md()),
-                       v2.md().begin(), rk_update2);
-    dr::mhp::transform(dr::mhp::views::zip(e.md(), e1.md(), dedt.md()),
-                       e2.md().begin(), rk_update2);
-    dr::mhp::halo(u2.md()).exchange();
-    dr::mhp::halo(v2.md()).exchange();
-    dr::mhp::halo(e2.md()).exchange();
+    dr::mhp::transform(dr::mhp::views::zip(u, u1, dudt), u2.begin(),
+                       rk_update2);
+    dr::mhp::transform(dr::mhp::views::zip(v, v1, dvdt), v2.begin(),
+                       rk_update2);
+    dr::mhp::transform(dr::mhp::views::zip(e, e1, dedt), e2.begin(),
+                       rk_update2);
+    dr::mhp::halo(u2).exchange();
+    dr::mhp::halo(v2).exchange();
+    dr::mhp::halo(e2).exchange();
 
     // RK stage 3: u3 = 1/3*u + 2/3*(u2 + dt*rhs(u2))
     rhs(u2, v2, e2, dudx, dvdy, dudt, dvdt, dedt, g, h, dx_inv, dy_inv, dt);
-    dr::mhp::transform(dr::mhp::views::zip(u.md(), u2.md(), dudt.md()),
-                       u.md().begin(), rk_update3);
-    dr::mhp::transform(dr::mhp::views::zip(v.md(), v2.md(), dvdt.md()),
-                       v.md().begin(), rk_update3);
-    dr::mhp::transform(dr::mhp::views::zip(e.md(), e2.md(), dedt.md()),
-                       e.md().begin(), rk_update3);
-    dr::mhp::halo(u.md()).exchange();
-    dr::mhp::halo(v.md()).exchange();
-    dr::mhp::halo(e.md()).exchange();
+    dr::mhp::transform(dr::mhp::views::zip(u, u2, dudt), u.begin(), rk_update3);
+    dr::mhp::transform(dr::mhp::views::zip(v, v2, dvdt), v.begin(), rk_update3);
+    dr::mhp::transform(dr::mhp::views::zip(e, e2, dedt), e.begin(), rk_update3);
+    dr::mhp::halo(u).exchange();
+    dr::mhp::halo(v).exchange();
+    dr::mhp::halo(e).exchange();
   }
   auto toc = std::chrono::steady_clock::now();
   std::chrono::duration<double> duration = toc - tic;
@@ -301,24 +281,25 @@ int run(int n, bool benchmark_mode) {
   }
 
   // Compute error against exact solution
-  array2d<T> e_exact(nx + 1, ny, 0, dist);
-  array2d<T> error(nx + 1, ny, 0, dist);
-  for (std::size_t i = 1; i < e_exact.nx(); i++) {
-    for (std::size_t j = 0; j < e_exact.ny(); j++) {
+  Array e_exact({nx + 1, ny}, dist);
+  dr::mhp::fill(e_exact, 0.0);
+  Array error({nx + 1, ny}, dist);
+  for (std::size_t i = 1; i < e_exact.mdspan().extent(0); i++) {
+    for (std::size_t j = 0; j < e_exact.mdspan().extent(1); j++) {
       T x = xmin + dx / 2 + (i - 1) * dx;
       T y = ymin + dy / 2 + j * dy;
-      e_exact(i, j) = exact_elev(x, y, t, lx, ly);
+      e_exact.mdspan()(i, j) = exact_elev(x, y, t, lx, ly);
     }
   }
-  dr::mhp::halo(e_exact.md()).exchange();
+  dr::mhp::halo(e_exact).exchange();
   auto error_kernel = [](auto ops) {
     auto err = ops.first - ops.second;
     return err * err;
   };
-  dr::mhp::transform(dr::mhp::views::zip(e.md(), e_exact.md()),
-                     error.md().begin(), error_kernel);
-  double err_L2 = dr::mhp::reduce(error.md(), static_cast<T>(0), std::plus{}) *
-                  dx * dy / lx / ly;
+  dr::mhp::transform(dr::mhp::views::zip(e, e_exact), error.begin(),
+                     error_kernel);
+  double err_L2 = dr::mhp::reduce(error, static_cast<T>(0), std::plus{}) * dx *
+                  dy / lx / ly;
   err_L2 = std::sqrt(err_L2);
   if (comm_rank == 0) {
     std::cout << "L2 error: " << std::setw(7) << std::scientific;
@@ -373,7 +354,7 @@ int main(int argc, char *argv[]) {
   cxxopts::Options options_spec(argv[0], "wave equation");
   // clang-format off
   options_spec.add_options()
-    ("n", "Grid size", cxxopts::value<std::std::size_t>()->default_value("128"))
+    ("n", "Grid size", cxxopts::value<std::size_t>()->default_value("128"))
     ("t,benchmark-mode", "Run a fixed number of time steps.", cxxopts::value<bool>()->default_value("false"))
     ("h,help", "Print help");
   // clang-format on
@@ -386,7 +367,7 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  std::std::size_t n = options["n"].as<std::std::size_t>();
+  std::size_t n = options["n"].as<std::size_t>();
   bool benchmark_mode = options["t"].as<bool>();
 
   auto error = run(n, benchmark_mode);
